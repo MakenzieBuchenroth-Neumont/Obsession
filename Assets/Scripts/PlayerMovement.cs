b1b031400b62c8dfb0f32dc53d6da23a54587c2b
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-10-01 12:59:37 -0600

    Fixed player movement and changed wonky animations.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] private Animator animator;
+	[SerializeField] private Rigidbody rb;
+	public float walkingSpeed, walkBackwardsSpeed, olwSpeed, runSpeed, roSpeed;
+	public bool walking;
+	public Transform playerTrans;
+
+	private void FixedUpdate() {
+		if (Input.GetKey(KeyCode.W)) {
+			rb.velocity = transform.forward * walkingSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			rb.velocity = -transform.forward * walkBackwardsSpeed * Time.deltaTime;
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.W)) {
+			animator.SetTrigger("walk");
+			animator.ResetTrigger("idle");
+			walking = true;
+		}
+		if (Input.GetKeyUp(KeyCode.W)) {
+			animator.ResetTrigger("walk");
+			animator.SetTrigger("idle");
+			rb.velocity = Vector3.zero;
+			walking = false;
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			animator.SetTrigger("walkBack");
+			animator.ResetTrigger("idle");
+		}
+		if (Input.GetKeyUp(KeyCode.S)) {
+			animator.ResetTrigger("walkBack");
+			animator.SetTrigger("idle");
+			rb.velocity = Vector3.zero;
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			playerTrans.Rotate(0, -roSpeed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			playerTrans.Rotate(0, roSpeed * Time.deltaTime, 0);
+		}
+		if (walking == true) {
+			if (Input.GetKeyDown(KeyCode.LeftShift)) {
+				walkingSpeed = walkingSpeed + runSpeed;
+				animator.SetTrigger("run");
+				animator.ResetTrigger("walk");
+			}
+			if (Input.GetKeyUp(KeyCode.LeftShift)) {
+				walkingSpeed = olwSpeed;
+				animator.ResetTrigger("run");
+				animator.SetTrigger("walk");
+			}
+		}
+	}
+}
